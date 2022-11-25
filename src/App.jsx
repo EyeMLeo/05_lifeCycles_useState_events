49b88cbea56a45420272da63d63442f968ab211f
EyeMLeo <Leonid.Akuta@gmail.com>
2022-11-25 12:45:47 +0100

    05 lifeCycle, events, loops counter test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
-
 import './App.css';
+import Title from './components/Title';
+import MainTitle from './components/MainTitle';
+import Counter from './components/counters/Counter';
+import Users from './components/Users';
 
 function App() {
+  console.log('App.jsx created');
+
+  // paspaudus an h1 el, padidini jo dydi iki 2rem
   return (
-    <div className="App">
-      <h1>life cycle</h1>
+    <div className="App container">
+      <MainTitle />
+      <Counter counterTitle="Push ups" />
+      <Counter>Push ups</Counter>
+
+      {/* <Title>Events and Hooks</Title> */}
+      {/* <Users /> */}
     </div>
   );
 }
---
+++ src/components/counters/Counter.jsx
@@ -0,0 +1,61 @@
+import './counter.css';
+import { useState } from 'react';
+
+function Counter(props) {
+  // 1. state counterValue, useState
+  const [counterValue, setCounterValue] = useState(0);
+  // susikurti counterClass state
+  // const [counterClass, setCounterClass] = useState('');
+
+  // kai turim reiksme kuri tiesiogiai priklauso nuo kitos reiksme, galim naudoti
+  // infered/caclulated value
+  let counterClassInfered = counterValue >= 5 ? 'high' : '';
+  let counterClassInferedLow = counterValue <= -5 ? 'low' : '';
+
+  // const counterValue = stateArr[0];
+  // const setCounterValue = stateArr[1];
+  // 2. increaseHandler, decreaseHandler fn kurios didins ir mazins skaitliuka
+  function incrementHandler() {
+    setCounterValue((prevCounterValue) => {
+      return prevCounterValue + 1;
+    });
+
+    // pasitikirinti reiksme ar daugiau uz 5 jei taip, pridedam klase
+    // if (counterValue >= 4) {
+    // setCounterClass('high');
+    // }
+  }
+
+  function minuzsHandler() {
+    setCounterValue((prevCounterValue) => {
+      return prevCounterValue <= -5 ? prevCounterValue : prevCounterValue - 1;
+    });
+  }
+
+  function resetHandler() {
+    setCounterValue(() => 0);
+  }
+
+  // let reiksme = 10;
+  // reiksme++; // === reiksme = reiksme + 1
+  // reksme + 1; //  reksme + 1
+
+  // kai reiksme virsija 5 uzdedam counter__value prideti klase high
+
+  return (
+    <div className="counter card">
+      <h3>{props.counterTitle}</h3>
+      <h2
+        className={`counter__value ${counterClassInferedLow} ${counterClassInfered}`}
+      >
+        {counterValue}
+      </h2>
+      <div className="control">
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={minuzsHandler}>-</button>
+        <button onClick={resetHandler}>reset</button>
+      </div>
+    </div>
+  );
+}
+export default Counter;
